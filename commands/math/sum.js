2e08dc73224b4627db51a49346250aaa55084d84
ItsDeadEnd <adrianppreyra@gmail.com>
2017-05-27 08:33:03 +0000

    this doesnt work yet

---
+++ commands/math/sum.js
@@ -0,0 +1,32 @@
+const commando = require('discord.js-commando');
+
+module.exports = class sum extends commando.Command{
+    constructor(client){
+        super(client,   {
+                            name: 'sum',
+                            aliases: ['add'],
+                            group: 'math',
+                            memberName: 'sum',
+                            description: 'Add a number to another number. exciting stuff...',
+                            examples: ['add 1 1'],
+                            args: [
+                                {
+                                    key: 'num1',
+                                    //label: 'num1',
+                                    prompt: 'the first of the two numbers to be added.',
+                                    type: 'float'
+                                },
+                                {
+                                    key: 'num2',
+                                    prompt: 'the second of the two numbers to be added.',
+                                    type: 'float'
+                                }
+                            ]
+        });
+    }
+
+    async run(msg, args){
+        var sum = args['num1'] + args['num2'];
+        return 'the sum of ' + args['num1'] + ' and ' + args['num2'] + ' is ' + sum;
+    }
+}
