39adaf41ab9f70d36b5aee4657e1801852c34852
ItsDeadEnd <adrianppreyra@gmail.com>
2017-05-27 02:26:36 +0000

    removed contents of bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-//todo: add a discord library
-
-//todo: create bot as discord client
-
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
-bot.login('MzE1NjkwNDQ3OTU3Nzg2NjI1.DApixg.NsW3haYKKyXB1W_iSvchBz9mxno');
