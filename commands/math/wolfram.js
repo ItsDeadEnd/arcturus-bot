860e531f2c1301ad9363adc39358b1ab4528ecb8
ItsDeadEnd <adrianppreyra@gmail.com>
2017-05-29 23:15:59 +0000

    added a wolfram query command

---
+++ commands/math/wolfram.js
@@ -0,0 +1,18 @@
+const commando = require('discord.js-commando');
+
+module.exports = class Wolfram extends commando.Command{
+    constructor(client){
+        super(client,   {
+                            name: 'wolfram',
+                            aliases: ['solve'],
+                            group: 'math',
+                            memberName: 'wolfram',
+                            description: 'Ask WolframAlpha a question',
+                            examples: ['wolfram 2*2']
+                        });
+    }
+
+    async run(msg, args){
+
+    }
+}
